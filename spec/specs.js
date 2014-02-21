0c475e7ff9feb54b21ea1ce9ab74732b6a90ae89
Maureen & Bailey <student@epicodus.com>
2014-02-21 11:46:30 -0800

    getValueLetter using IndexOf

--- spec/specs.js
+++ spec/specs.js
@@ -28,3 +28,17 @@ describe('getLetterValue', function() {
   });
 });
 
+describe('scrabble', function() {
+  it('returns score for the sum of letters in word', function() {
+    scrabble('cat').should.equal(5);
+  });
+
+  it('returns score for the sum of letters in word regardless of case', function() {
+    scrabble('Mississippi').should.equal(17);
+  });
+
+  it('returns an error if a character other than a letter is entered', function() {
+    scrabble('HappyBirthday!').should.equal("Error: please enter letters only");
+  });
+});
+
