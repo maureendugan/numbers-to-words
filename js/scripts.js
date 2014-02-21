0991df8f85f2754b58d87fb5e9f941e590a7c353
Maureen & Bailey <student@epicodus.com>
2014-02-21 11:49:57 -0800

    scrabble using object alphabet

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,10 @@
-var getLetterValue = function(letter) {
-  var valueOneLetters = ['a', 'e', 'i', 'o', 'u' , 'l' , 'n' , 'r', 's', 't'];
-  var valueTwoLetters = ['d','g'];
-  var valueThreeLetters = ['b','c', 'm', 'p']; 
-  var valueFourLetters = ['f', 'h','v', 'w', 'y']; 
-  var valueEightLetters = ['j', 'x'];
-  var valueTenLetters = ['q', 'z'];
-  var result;
-
-  if (letter === 'k') {
-    return 5;
-  } else if (valueOneLetters.indexOf(letter) >= 0) {
-    return 1;
-  } else if (valueTwoLetters.indexOf(letter) >= 0) {
-    return 2;
-  } else if (valueThreeLetters.indexOf(letter) >= 0){
-    return 3;
-  } else if (valueFourLetters.indexOf(letter) >=0){
-    return 4;
-  } else if (valueEightLetters.indexOf(letter) >=0){
-    return 8;
-  } else if (valueTenLetters.indexOf(letter) >=0){
-    return 10;
-  }
-};  
-
 var scrabble = function(word) {
   var word = word.toLowerCase().split('');
   var score = 0;
   var alphabet = {a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, r: 1, s: 1, t: 1, d: 2, g: 2, b: 3, c: 3, m: 3, p: 3, f: 4, h: 4, v: 4, w: 4, y: 4, k: 5, j: 8, x: 8, q: 10, z: 10};
 
   for (var i = 0; i < word.length; i ++) {
-    score += getLetterValue(word[i]);
+    score += alphabet[word[i]];
   }
   if (score > 0){
     return score;
--- spec/specs.js
+++ spec/specs.js
@@ -1,33 +1,3 @@
-describe('getLetterValue', function() {
-  it('returns a score of one for the letter a', function() {
-    getLetterValue('a').should.equal(1);
-  });
-
-  it('returns 2 for the letter g', function() {
-    getLetterValue('g').should.equal(2);
-  });
-
-  it('returns 3 for the letter b', function() {
-    getLetterValue('b').should.equal(3);
-  });
-
-  it('returns 4 for the letter v', function() {
-    getLetterValue('v').should.equal(4);
-  });
-
-  it('returns 5 for the letter k', function() {
-    getLetterValue('k').should.equal(5);
-  });
-
-  it('returns 8 for the letter x', function() {
-    getLetterValue('x').should.equal(8);
-  });
-
-  it('returns 10 for the letter z', function() {
-    getLetterValue('z').should.equal(10);
-  });
-});
-
 describe('scrabble', function() {
   it('returns score for the sum of letters in word', function() {
     scrabble('cat').should.equal(5);
